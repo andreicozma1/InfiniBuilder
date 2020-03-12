5d5644fb459e1446caaf39934c6fdc03cce5242b
hunter price <hunterprice4044@gmail.com>
2020-03-12 11:43:39 -0400

    Began creating class to randomly generate mazes named MazeGenerator.java(10min)

---
+++ src/sample/MazeGenerator.java
@@ -0,0 +1,30 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class MazeGenerator {
+    private int row;
+    private int col;
+    private int seed;
+    private ArrayList<ArrayList<Boolean>> maze;
+
+    public MazeGenerator(int row, int col, int seed){
+        setRow(row);
+        setCol(col);
+        setSeed(seed);
+        maze = new ArrayList<ArrayList<Boolean>>();
+
+        for( int i = 0 ; i < row ; i++ ){
+            ArrayList<Boolean>tmp = new ArrayList<Boolean>(col);
+            maze.add(tmp);
+        }
+    }
+
+    public void setCol(int col) { this.col = col; }
+    public void setRow(int row) { this.row = row; }
+    public void setSeed(int seed) { this.seed = seed; }
+
+    public int getCol() { return col; }
+    public int getRow() { return row; }
+    public int getSeed() { return seed; }
+}
