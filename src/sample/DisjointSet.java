332fb79615661b4a114e1f56a2dded33daa5d9ec
hunter price <hunterprice4044@gmail.com>
2020-03-12 13:02:02 -0400

    implemented disjointset using path compression in java (20 min)

--- src/sample/DisjointSet.java
+++ src/sample/DisjointSet.java
@@ -21,14 +21,57 @@ public class DisjointSet{
             System.out.println("Must call union on a set, not just an element.");
             return -1;
         }
-        return -1;
+
+        if( ranks.get(s1) > ranks.get(s2) ){
+            parent = s1;
+            child = s2;
+        } else {
+            parent = s2;
+            child = s1;
+        }
+
+        links.set(child,parent);
+        if(ranks.get(parent) == ranks.get(child)){
+            ranks.set(parent,ranks.get(parent)+1);
+        }
+        return parent;
     }
 
     public int Find(int element){
-        return -1;
+        // declare parent and child node variables
+        int parent, child;
+
+        // find the root of the tree
+        // while finding the root set the parents link to the childs
+        child = -1;
+        while ( links.get(element) != -1 ){
+            parent = links.get(element);
+            links.set(element,child);
+            child = element;
+            element = parent;
+        }
+
+        // travels back to original element
+        // set each node link to the root of the tree
+        parent = element;
+        element = child;
+        while (element != -1){
+            child = links.get(element);
+            links.set(element, parent);
+            element = child;
+        }
+        return parent;
     }
 
     public void Print(){
+        int i;
 
+        System.out.println("\nNode:  ");
+        for (i = 0; i < links.size(); i++) System.out.println(i + " ");
+        System.out.println("\nLinks:  ");
+        for (i = 0; i < links.size(); i++) System.out.println(links.get(i) + " ");
+        System.out.println("\nRanks:  ");
+        for (i = 0; i < links.size(); i++) System.out.println(ranks.get(i) + " ");
+        System.out.println("\n");
     }
 }
