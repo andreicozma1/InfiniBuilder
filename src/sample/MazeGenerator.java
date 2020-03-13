30330bc14ce2f0071e2c1c45b5374fe6971234a3
hunter price <hunterprice4044@gmail.com>
2020-03-13 17:09:19 -0400

    MazeGenrator will now generate a set of walls of the maze(40 min)

--- src/sample/MazeGenerator.java
+++ src/sample/MazeGenerator.java
@@ -1,100 +1,136 @@
 package sample;
 
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.Random;
 
 public class MazeGenerator {
-    private int row;
-    private int col;
+    private int rows;
+    private int cols;
     private long seed;
-    private int startingXCoord = 0 ;
-    private int startingYCoord = 0 ;
-    private int startingZCoord = 0 ;
-    private int finalXCoord = 0 ;
-    private int finalYCoord = 0 ;
-    private int finalZCoord = 0 ;
+    private int startingXCoord = 0;
+    private int startingYCoord = 0;
+    private int startingZCoord = 0;
+    private int finalXCoord = 0;
+    private int finalYCoord = 0;
+    private int finalZCoord = 0;
     private Random ran;
-    private ArrayList<ArrayList<Boolean>> maze;
+    private ArrayList<Boolean> maze;
     private ConcurrentSkipListMap<Double, Integer> walls;
     private DisjointSet disjointSet;
 
-    public MazeGenerator(int row, int col, long seed){
+    public MazeGenerator(int row, int col, long seed) {
         ran = new Random();
-        setRow(row);
-        setCol(col);
+        setRows(row);
+        setCols(col);
         setSeed(seed);
-        maze = new ArrayList<ArrayList<Boolean>>();
+        maze = new ArrayList<Boolean>();
         walls = new ConcurrentSkipListMap<Double, Integer>();
-        disjointSet = new DisjointSet(this.row*this.col);
+        disjointSet = new DisjointSet(this.rows * this.cols);
 
         generateWalls();
         breakWalls();
+        makeMaze();
+
     }
 
-    public void setCol(int col) { this.col = col; }
-    public void setRow(int row) { this.row = row; }
+    public void setCols(int cols) {
+        this.cols = cols;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
     public void setSeed(long seed) {
         this.seed = seed;
         ran.setSeed(this.seed);
     }
 
-    public int getCol() { return col; }
-    public int getRow() { return row; }
-    public long getSeed() { return seed; }
+    public int getCols() {
+        return cols;
+    }
 
-    private void generateWalls(){
+    public int getRows() {
+        return rows;
+    }
+
+    public long getSeed() {
+        return seed;
+    }
+
+    private void generateWalls() {
 
         int r, c, c1;
 
         // generate walls that separate vertical cells
-        for( r = 0; r < row-1; r++ ){
-            for ( c = 0; c < col; c++ ){
-                c1 = r*col + c;
-                walls.put(ran.nextDouble(),c1);
+        for (r = 0; r < rows - 1; r++) {
+            for (c = 0; c < cols; c++) {
+                c1 = r * cols + c;
+                walls.put(ran.nextDouble(), c1);
             }
         }
 
         // generate walls that separate horizontal cells
-        for( r = 0; r < row; r++ ){
-            for ( c = 0; c < col-1; c++ ){
-                c1 = (r*col + c) + row*col;
-                walls.put(ran.nextDouble(),c1);
+        for (r = 0; r < rows; r++) {
+            for (c = 0; c < cols - 1; c++) {
+                c1 = (r * cols + c) + rows * cols;
+                walls.put(ran.nextDouble(), c1);
             }
         }
     }
 
-    private void breakWalls(){
+    private void breakWalls() {
 
         int c1, c2, ncomp, s1, s2;
         Map.Entry wallEntry;
 
-        ncomp = row * col;
-        wallEntry = walls.pollFirstEntry();
-        while( ncomp > 1 ){
-            c1 = (int)wallEntry.getValue();
+        ncomp = rows * cols;
+        wallEntry = walls.firstEntry();
+        while (ncomp > 1) {
+            c1 = (int) wallEntry.getValue();
             // wall separating vertical cells
-            if ( c1 < row * col ){
-                c2 = c1 + col;
+            if (c1 < rows * cols) {
+                c2 = c1 + cols;
             }
             // wall separating horizontal cells
             else {
-                c1 -= row * col;
-                c2 = c1 +1;
+                c1 -= rows * cols;
+                c2 = c1 + 1;
             }
+
             s1 = disjointSet.Find(c1);
             s2 = disjointSet.Find(c2);
-            if(s1 != s2){
-                disjointSet.Union(s1,s2);
-                //finish this part
 
+             // test for different connected parts
+            if (s1 != s2) {
+                disjointSet.Union(s1, s2);
                 wallEntry = walls.pollFirstEntry();
                 ncomp--;
-            }else{
-                wallEntry = walls.pollFirstEntry();
+            } else {
+                wallEntry = walls.higherEntry((Double) wallEntry.getKey());
             }
         }
+    }
 
+    private void makeMaze(){
+
+    }
+
+    public void printWalls(){
+
+        int c1, c2;
+
+        System.out.println("ROWS " + rows + " COLS " + cols);
+
+        for(Integer n : walls.values()){
+            c1 = n;
+            if( c1 < rows*cols){
+                c2 = c1 + cols;
+            } else {
+                c1 -= rows*cols;
+                c2 = c1 + 1;
+            }
+            System.out.println("WALL "+ c1 + " " + c2);
+        }
     }
 }
