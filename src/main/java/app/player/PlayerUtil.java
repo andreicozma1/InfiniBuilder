c1eb2e57029ac5209be1907dec349bee83a33cd5
Andrei Cozma <andrei.cozma@elotouch.com>
2020-04-01 00:27:43 -0500

    Added

--- src/main/java/app/player/PlayerUtil.java
+++ src/main/java/app/player/PlayerUtil.java
@@ -128,6 +128,8 @@ public class PlayerUtil {
 
     public void placeObject() {
         StructureBuilder cb = ((Inventory)context.getHUD().getElement(HUDUtil.INVENTORY)).getInventoryUtil().popCurrentItem();
+       context.getHUD().getElement(HUDUtil.INVENTORY).update();
+
         if(cb.getItemTag() != StructureBuilder.UNDEFINED_TAG){
             context.getEnvironment().placeObject(getPoint2D(), cb, true);
         }
